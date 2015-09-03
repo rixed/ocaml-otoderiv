abaa6a3982e798e106962e252ab02942ff457ca1
Cedric Cellier <rixed@happyleptic.org>
2015-09-03 20:27:56 +0100

    Make the implementation of the structure containing the values abstract.

--- check.ml
+++ check.ml
@@ -3,19 +3,30 @@ open Algen_impl
 open OUnit2
 
 module I = IntField (Dim1)
-module A = AutoDeriv (I)(struct let dim = 1 end)
-module A2 = AutoDeriv (I)(struct let dim = 2 end)
+module T1 = TupleArray (I) (struct let dim = 1 end)
+module A1 = AutoDeriv (I) (T1)
+module T2 = TupleArray (I) (struct let dim = 2 end)
+module A2 = AutoDeriv (I) (T2)
 
 let _ =
   (* test function: x -> 3*x^2 *)
-  let fun1 x = A.(mul (of_int 3) (square x)) in
-  assert_equal (I.zero, [|I.zero|]) (A.apply fun1 I.zero) ;
-  Format.printf "%a\n" A.print (A.apply fun1 I.one) ;
-  assert_equal (I.of_int 3, [|I.of_int 6|])  (A.apply fun1 I.one) ;
-  Format.printf "%a\n" A.print (A.apply fun1 (I.of_int 3)) ;
-  assert_equal (I.of_int 27, [|I.of_int 18|]) (A.apply fun1 (I.of_int 3)) ;
+  let fun1 x = A1.(mul (of_int 3) (square x)) in
+  let v = A1.apply fun1 I.zero in
+  assert_equal (T1.value v) I.zero ;
+  assert_equal (T1.deriv 0 v) I.zero ;
+  let v = A1.apply fun1 I.one in
+  Format.printf "%a\n" A1.print v ;
+  assert_equal (I.of_int 3) (T1.value v) ;
+  assert_equal (I.of_int 6) (T1.deriv 0 v) ;
+  let v = A1.apply fun1 (I.of_int 3) in
+  Format.printf "%a\n" A1.print v ;
+  assert_equal (I.of_int 27) (T1.value v) ;
+  assert_equal (I.of_int 18) (T1.deriv 0 v) ;
   (* test function: x,y -> 3x+2y *)
-  let fun2 x y = A2.(add (mul (of_int 3) x) (mul (of_int 2) y)) in
+  let fun2 x y = A2.(add (mul (of_int 3) x)
+                         (mul (of_int 2) y)) in
   (* Evaluate this at (1,2): *)
-  assert_equal (I.of_int 7, [|I.of_int 3; I.of_int 2|])
-               (A2.apply2 fun2 (I.of_int 1) (I.of_int 2))
+  let v = A2.apply2 fun2 (I.of_int 1) (I.of_int 2) in
+  assert_equal (I.of_int 7) (T2.value v) ;
+  assert_equal (I.of_int 3) (T2.deriv 0 v) ;
+  assert_equal (I.of_int 2) (T2.deriv 1 v)
--- otoderiv.ml
+++ otoderiv.ml
@@ -1,97 +1,141 @@
 open Algen_intf
 
-module type CONFIG = sig
-  val dim : int
+module type TUPLE =
+sig
+  type t
+  type v
+
+  (* Return the tuple made of the given value and all derivatives zeroed *)
+  val deriv0 : v -> t
+  (* Same as above, but nth derivative is one *)
+  val deriv1 : v -> int -> t
+
+  (* Extract the value out of the tuple *)
+  val value : t -> v
+
+  (* Extract the derivative along the nth direction *)
+  val deriv : int -> t -> v
+
+  (* Combine the two tuple with the given functions, one combining the values
+   * and the other the derivatives. *)
+  val combine : t -> t -> (v -> v -> v) -> (v -> v -> v) -> t
+
+  (* Apply the given functions to the tuple. *)
+  val apply : t -> (v -> v) -> (v -> v) -> t
+
+  val print : Format.formatter -> t -> unit
 end
 
-module AutoDeriv_Group (B : CORE_GROUP) (Conf : CONFIG) :
-  CORE_GROUP with type t = B.t * B.t array =
+module type TUPLE_CONFIG = sig val dim : int end
+module TupleArray (B : CORE_RING) (Conf : TUPLE_CONFIG) :
+  TUPLE with type v = B.t =
 struct
-  type t = B.t * B.t array
+  type v = B.t
+  type t = v array (* 0 being the value and the rest for the derivatives *)
 
-  let zero = B.zero, Array.make Conf.dim B.zero
+  let deriv1 v n =
+    Array.init (succ Conf.dim) (fun i ->
+      if i = 0 then v else if i = succ n then B.one else B.zero)
+  let deriv0 v = deriv1 v ~-1
 
-  let add (a, a') (b, b') =
-    B.add a b,
-    Array.init Conf.dim (fun i -> B.add a'.(i) b'.(i))
+  let value t = t.(0)
+  let deriv n t = t.(n+1)
 
-  let neg (a, a') =
-    B.neg a,
-    Array.map B.neg a'
+  let combine t1 t2 fv fd =
+    Array.init (succ Conf.dim) (fun i -> (if i = 0 then fv else fd) t1.(i) t2.(i))
 
-  let compare (a, _) (b, _) = B.compare a b
+  let apply t fv fd =
+    Array.init (succ Conf.dim) (fun i -> (if i = 0 then fv else fd) t.(i))
 
-  let array_print fmt a =
+  let print fmt a =
     Array.iteri (fun i v ->
       if i > 0 then Format.pp_print_space fmt () ;
       B.print fmt v) a
-
-  let print fmt (a, a') =
-    Format.fprintf fmt "(%a, %a)" B.print a array_print a'
 end
 
-module AutoDeriv_Ring (B : CORE_RING) (Conf : CONFIG) :
-  CORE_RING with type t = B.t * B.t array =
+module AutoDeriv_Group (B : CORE_GROUP) (T : TUPLE with type v = B.t) :
+  CORE_GROUP with type t = T.t =
 struct
-  include Group(AutoDeriv_Group(B)(Conf))
+  type t = T.t
 
-  let one = B.one, Array.make Conf.dim B.zero
+  let zero = T.deriv0 B.zero
 
-  let mul (a, a') (b, b') =
-    B.mul a b,
-    Array.init Conf.dim (fun i -> B.add (B.mul a b'.(i)) (B.mul a'.(i) b))
+  let add a b = T.combine a b B.add B.add
+
+  let neg a = T.apply a B.neg B.neg
+
+  let compare a b = B.compare (T.value a) (T.value b)
+
+  let print fmt a =
+    Format.fprintf fmt "%a" T.print a
 end
 
-module AutoDeriv_Field (B : CORE_FIELD) (Conf : CONFIG) :
-  CORE_FIELD with type t = B.t * B.t array =
+module AutoDeriv_Ring (B : CORE_RING) (T : TUPLE with type v = B.t) :
+  CORE_RING with type t = T.t =
+struct
+  include Group(AutoDeriv_Group(B)(T))
+
+  let one = T.deriv0 B.one
+
+  let mul a b =
+    T.combine a b B.mul
+      (fun a' b' -> B.add (B.mul (T.value a) b')
+                          (B.mul a' (T.value b)))
+end
+
+module AutoDeriv_Field (B : CORE_FIELD) (T : TUPLE with type v = B.t) :
+  CORE_FIELD with type t = T.t =
 struct
   module F = Field(B)
-  include Ring(AutoDeriv_Ring(B)(Conf))
+  include Ring(AutoDeriv_Ring(B)(T))
 
-  let inv (a, a') =
-    B.inv a,
-    Array.init Conf.dim (fun i -> F.div a'.(i) (B.neg (B.inv (B.square a))))
+  let inv a =
+    T.apply a B.inv
+      (fun a' -> F.div a' (B.neg (B.inv (B.square (T.value a)))))
 
-  let sqrt (a, a') =
-    B.sqrt a,
-    Array.init Conf.dim (fun i -> F.div a'.(i) (B.double (B.sqrt a)))
+  let sqrt a =
+    T.apply a B.sqrt (fun a' ->
+      F.div a' (B.double (B.sqrt (T.value a))))
 
-  let half (a, a') = B.half a, Array.init Conf.dim (fun i -> B.half a'.(i))
+  let half a = T.apply a B.half B.half
 
-  let floor (a, a') = B.floor a, a'
-  let ceil (a, a') = B.ceil a, a'
-  let rand (a, a') = B.rand a, Array.init Conf.dim (fun i -> B.rand a'.(i))
+  let identity x = x
+  let floor a = T.apply a B.floor identity
+  let ceil a = T.apply a B.ceil identity
+  let rand a = T.apply a B.rand B.rand
 
-  let of_int n = B.of_int n, Array.make Conf.dim B.zero
-  let to_int (a, _) = B.to_int a
-  let of_float f = B.of_float f, Array.make Conf.dim B.zero
-  let to_float (a, _) = B.to_float a
-  let of_nativeint n = B.of_nativeint n, Array.make Conf.dim B.zero
-  let to_nativeint (a, _) = B.to_nativeint a
-  let of_int64 n = B.of_int64 n, Array.make Conf.dim B.zero
-  let to_int64 (a, _) = B.to_int64 a
-  let of_string n = B.of_string n, Array.make Conf.dim B.zero
-  let to_string (a, _) = B.to_string a
+  let of_int n = T.deriv0 (B.of_int n)
+  let to_int a = B.to_int (T.value a)
+  let of_float f = T.deriv0 (B.of_float f)
+  let to_float a = B.to_float (T.value a)
+  let of_nativeint n = T.deriv0 (B.of_nativeint n)
+  let to_nativeint a = B.to_nativeint (T.value a)
+  let of_int64 n = T.deriv0 (B.of_int64 n)
+  let to_int64 a = B.to_int64 (T.value a)
+  let of_string n = T.deriv0 (B.of_string n)
+  let to_string a = B.to_string (T.value a)
 end
 
-module AutoDeriv_Trigo (B : TRIGO) (Conf : CONFIG) = struct
-  type t = B.t * B.t array
-  let pi = B.pi, Array.make Conf.dim B.zero
-  let sin (a, a') = B.sin a, Array.init Conf.dim (fun _ -> B.mul a' (B.cos a))
-  let cos (a, a') = B.cos a, Array.init Conf.dim (fun _ -> B.mul a' (B.neg (B.sin a)))
-  let tan (a, a') = B.tan a, Array.init Conf.dim (fun _ -> B.mul a' (B.sub (B.square (B.tan a)) B.one))
+module AutoDeriv_Trigo (B : TRIGO) (T : TUPLE with type v = B.t) = struct
+  type t = T.t
+  let pi = T.deriv0 B.pi
+  let sin a =
+    T.apply a B.sin (fun a' -> B.mul a' (B.cos (T.value a)))
+  let cos a =
+    T.apply a B.cos (fun a' -> B.mul a' (B.neg (B.sin (T.value a))))
+
+  let tan a =
+    T.apply a B.tan (fun a' -> B.mul a' (B.sub (B.square (B.tan (T.value a))) B.one))
 end
 
-module AutoDeriv (B : CORE_FIELD) (Conf : CONFIG) = struct
-  include Field(AutoDeriv_Field(B)(Conf))
+module AutoDeriv (B : CORE_FIELD) (T : TUPLE with type v = B.t) = struct
+  include Field(AutoDeriv_Field(B)(T))
 
   (* FIXME: could take the order of derivation we want as a parameter:
    * 0 for f(x), 1 for f'(x), 2 for f''(x), and so on. We would then compute
    * lazily that derivative (with memoization). *)
-  let apply f (x : B.t) =
-    f (x, Array.init Conf.dim (fun i -> if i == 0 then B.one else B.zero))
+  let apply f (x : B.t) = f (T.deriv1 x 0)
 
   let apply2 f (x : B.t) (y : B.t) =
-    f (x, [|B.one; B.zero|])
-      (y, [|B.zero; B.one|])
+    f (T.deriv1 x 0) (T.deriv1 y 1)
 end
