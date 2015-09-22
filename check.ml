3bea662af8fe22bb9d30932a7f3dd9ec1000309f
Cedric Cellier <rixed@happyleptic.org>
2015-09-22 05:56:06 +0100

    Add a VECTOR in the Tuple for function arguments.

--- check.ml
+++ check.ml
@@ -3,30 +3,38 @@ open Algen_impl
 open OUnit2
 
 module I = IntField (Dim1)
-module T1 = TupleArray (I) (struct let dim = 1 end)
+module T1 = TupleArray (I) (struct let v = 1 end)
 module A1 = AutoDeriv (I) (T1)
-module T2 = TupleArray (I) (struct let dim = 2 end)
+module T2 = TupleArray (I) (struct let v = 2 end)
 module A2 = AutoDeriv (I) (T2)
 
-let _ =
+let test1 _ =
   (* test function: x -> 3*x^2 *)
-  let fun1 x = A1.(mul (of_int 3) (square x)) in
-  let v = A1.apply fun1 I.zero in
-  assert_equal (T1.value v) I.zero ;
-  assert_equal (T1.deriv 0 v) I.zero ;
-  let v = A1.apply fun1 I.one in
-  Format.printf "%a\n" A1.print v ;
-  assert_equal (I.of_int 3) (T1.value v) ;
-  assert_equal (I.of_int 6) (T1.deriv 0 v) ;
-  let v = A1.apply fun1 (I.of_int 3) in
-  Format.printf "%a\n" A1.print v ;
-  assert_equal (I.of_int 27) (T1.value v) ;
-  assert_equal (I.of_int 18) (T1.deriv 0 v) ;
+  let f x = A1.(mul (of_int 3) (square x)) in
+  let v = A1.apply1 f I.zero in
+  assert_equal ~msg:"f(0)" ~printer:I.to_string (T1.value v) I.zero ;
+  assert_equal ~msg:"df/dx(0)" ~printer:I.to_string (T1.deriv 0 v) I.zero ;
+  let v = A1.apply1 f I.one in
+  assert_equal ~msg:"f(1)" ~printer:I.to_string (I.of_int 3) (T1.value v) ;
+  assert_equal ~msg:"df/dx(1)" ~printer:I.to_string (I.of_int 6) (T1.deriv 0 v) ;
+  let v = A1.apply1 f (I.of_int 3) in
+  assert_equal ~msg:"f(3)" ~printer:I.to_string (I.of_int 27) (T1.value v) ;
+  assert_equal ~msg:"df/dx(3)" ~printer:I.to_string (I.of_int 18) (T1.deriv 0 v)
+
+let test2 _ =
   (* test function: x,y -> 3x+2y *)
-  let fun2 x y = A2.(add (mul (of_int 3) x)
+  let f x y = A2.(add (mul (of_int 3) x)
                          (mul (of_int 2) y)) in
   (* Evaluate this at (1,2): *)
-  let v = A2.apply2 fun2 (I.of_int 1) (I.of_int 2) in
-  assert_equal (I.of_int 7) (T2.value v) ;
-  assert_equal (I.of_int 3) (T2.deriv 0 v) ;
-  assert_equal (I.of_int 2) (T2.deriv 1 v)
+  let v = A2.apply2 f (I.of_int 1) (I.of_int 2) in
+  assert_equal ~msg:"f(1, 2)" ~printer:I.to_string (I.of_int 7) (T2.value v) ;
+  assert_equal ~msg:"df/dx(1, 2)" ~printer:I.to_string (I.of_int 3) (T2.deriv 0 v) ;
+  assert_equal ~msg:"df/dy(1, 2)" ~printer:I.to_string (I.of_int 2) (T2.deriv 1 v)
+
+let deriv_test =
+  "derivation computation" >:::
+    [ "1 param function" >:: test1 ;
+      "2 params function" >:: test2 ]
+
+let () =
+  run_test_tt_main deriv_test
--- otoderiv.ml
+++ otoderiv.ml
@@ -4,6 +4,8 @@ module type TUPLE =
 sig
   type t
   type v
+  module K : FIELD with type t = v
+  module V : VECTOR with module K = K
 
   (* Return the tuple made of the given value and all derivatives zeroed *)
   val deriv0 : v -> t
@@ -26,94 +28,91 @@ sig
   val print : Format.formatter -> t -> unit
 end
 
-module type TUPLE_CONFIG = sig val dim : int end
-module TupleArray (B : CORE_RING) (Conf : TUPLE_CONFIG) :
-  TUPLE with type v = B.t =
+module TupleArray (K : FIELD) (Dim : CONF_INT) :
+  TUPLE with type v = K.t =
 struct
-  type v = B.t
+  type v = K.t
   type t = v array (* 0 being the value and the rest for the derivatives *)
+  module K = K
+  module V = Algen_vector.Make (K) (Dim)
 
   let deriv1 v n =
-    Array.init (succ Conf.dim) (fun i ->
-      if i = 0 then v else if i = succ n then B.one else B.zero)
+    Array.init (succ Dim.v) (fun i ->
+      if i = 0 then v else if i = succ n then K.one else K.zero)
   let deriv0 v = deriv1 v ~-1
 
   let value t = t.(0)
   let deriv n t = t.(n+1)
 
   let combine t1 t2 fv fd =
-    Array.init (succ Conf.dim) (fun i -> (if i = 0 then fv else fd) t1.(i) t2.(i))
+    Array.init (succ Dim.v) (fun i -> (if i = 0 then fv else fd) t1.(i) t2.(i))
 
   let apply t fv fd =
-    Array.init (succ Conf.dim) (fun i -> (if i = 0 then fv else fd) t.(i))
+    Array.init (succ Dim.v) (fun i -> (if i = 0 then fv else fd) t.(i))
 
   let print fmt a =
     Array.iteri (fun i v ->
       if i > 0 then Format.pp_print_space fmt () ;
-      B.print fmt v) a
+      K.print fmt v) a
 end
 
-module AutoDeriv_Group (B : CORE_GROUP) (T : TUPLE with type v = B.t) :
+module AutoDeriv_Group (K : CORE_GROUP) (T : TUPLE with type v = K.t) :
   CORE_GROUP with type t = T.t =
 struct
   type t = T.t
 
-  let zero = T.deriv0 B.zero
+  let zero = T.deriv0 K.zero
 
-  let add a b = T.combine a b B.add B.add
+  let add a b = T.combine a b K.add K.add
 
-  let neg a = T.apply a B.neg B.neg
+  let neg a = T.apply a K.neg K.neg
 
-  let compare a b = B.compare (T.value a) (T.value b)
+  let compare a b = K.compare (T.value a) (T.value b)
 
   let print fmt a =
     Format.fprintf fmt "%a" T.print a
 end
 
-module AutoDeriv_Ring (B : CORE_RING) (T : TUPLE with type v = B.t) :
-  CORE_RING with type t = T.t =
-struct
-  include Group(AutoDeriv_Group(B)(T))
-
-  let one = T.deriv0 B.one
-
-  let mul a b =
-    T.combine a b B.mul
-      (fun a' b' -> B.add (B.mul (T.value a) b')
-                          (B.mul a' (T.value b)))
-end
-
-module AutoDeriv_Field (B : CORE_FIELD) (T : TUPLE with type v = B.t) :
+module AutoDeriv_Field (K : CORE_FIELD) (T : TUPLE with type v = K.t) :
   CORE_FIELD with type t = T.t =
 struct
-  module F = Field(B)
-  include Ring(AutoDeriv_Ring(B)(T))
+  module F = Field(K)
+  include Ring(struct
+    include Group(AutoDeriv_Group(K)(T))
+
+    let one = T.deriv0 K.one
+
+    let mul a b =
+      T.combine a b K.mul
+        (fun a' b' -> K.add (K.mul (T.value a) b')
+                            (K.mul a' (T.value b)))
+  end)
 
   let inv a =
-    T.apply a B.inv
-      (fun a' -> F.div a' (B.neg (B.inv (B.square (T.value a)))))
+    T.apply a K.inv
+      (fun a' -> F.div a' (K.neg (K.inv (K.square (T.value a)))))
 
   let sqrt a =
-    T.apply a B.sqrt (fun a' ->
-      F.div a' (B.double (B.sqrt (T.value a))))
+    T.apply a K.sqrt (fun a' ->
+      F.div a' (K.double (K.sqrt (T.value a))))
 
-  let half a = T.apply a B.half B.half
+  let half a = T.apply a K.half K.half
 
   let identity x = x
-  let floor a = T.apply a B.floor identity
-  let ceil a = T.apply a B.ceil identity
-  let rand a = T.apply a B.rand B.rand
+  let floor a = T.apply a K.floor identity
+  let ceil a = T.apply a K.ceil identity
+  let rand a = T.apply a K.rand K.rand
 
-  let of_int n = T.deriv0 (B.of_int n)
-  let to_int a = B.to_int (T.value a)
-  let of_float f = T.deriv0 (B.of_float f)
-  let to_float a = B.to_float (T.value a)
-  let of_nativeint n = T.deriv0 (B.of_nativeint n)
-  let to_nativeint a = B.to_nativeint (T.value a)
-  let of_int64 n = T.deriv0 (B.of_int64 n)
-  let to_int64 a = B.to_int64 (T.value a)
-  let of_string n = T.deriv0 (B.of_string n)
-  let to_string a = B.to_string (T.value a)
+  let of_int n = T.deriv0 (K.of_int n)
+  let to_int a = K.to_int (T.value a)
+  let of_float f = T.deriv0 (K.of_float f)
+  let to_float a = K.to_float (T.value a)
+  let of_nativeint n = T.deriv0 (K.of_nativeint n)
+  let to_nativeint a = K.to_nativeint (T.value a)
+  let of_int64 n = T.deriv0 (K.of_int64 n)
+  let to_int64 a = K.to_int64 (T.value a)
+  let of_string n = T.deriv0 (K.of_string n)
+  let to_string a = K.to_string (T.value a)
 end
 
 module AutoDeriv_Trigo (B : TRIGO) (T : TUPLE with type v = B.t) = struct
@@ -128,14 +127,33 @@ module AutoDeriv_Trigo (B : TRIGO) (T : TUPLE with type v = B.t) = struct
     T.apply a B.tan (fun a' -> B.mul a' (B.sub (B.square (B.tan (T.value a))) B.one))
 end
 
-module AutoDeriv (B : CORE_FIELD) (T : TUPLE with type v = B.t) = struct
-  include Field(AutoDeriv_Field(B)(T))
+module AutoDeriv (K : CORE_FIELD) (T : TUPLE with type v = K.t) = struct
+  include Field(AutoDeriv_Field(K)(T))
 
   (* FIXME: could take the order of derivation we want as a parameter:
    * 0 for f(x), 1 for f'(x), 2 for f''(x), and so on. We would then compute
    * lazily that derivative (with memoization). *)
-  let apply f (x : B.t) = f (T.deriv1 x 0)
+  let apply1 f (x : K.t) = f (T.deriv1 x 0)
 
-  let apply2 f (x : B.t) (y : B.t) =
+  let apply2 f (x : K.t) (y : K.t) =
     f (T.deriv1 x 0) (T.deriv1 y 1)
+
+  let apply f (xs : K.t array) =
+    let nb_parms = Array.length xs in
+    f (Array.init nb_parms (fun i -> T.deriv1 xs.(i) i))
+
+  (* Minimization function: *)
+  let rec minimize eps f (x : T.V.t) =
+    let v = apply f x in
+    let search_dir = Array.init T.V.Dim.v (fun i -> K.neg (T.deriv i v)) in
+    let search_dir_len = T.V.norm search_dir in
+    let rec next_x c =
+      if K.mul c search_dir_len < eps then x else
+      let x' = T.V.add x (T.V.mul c search_dir) in 
+      let v' = apply f x' in
+      if T.value v' < K.sub (T.value v) (K.mul c search_dir_len) then x'
+      else next_x (K.half c) in
+    let x' = next_x (K.half K.one) in
+    if x' == x then x else (* physical identity here *)
+    minimize eps f x'
 end
